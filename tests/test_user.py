5ae820e0a463ec9ffed4c6be7308b421d3dbfb98
rodrigmars <rodrigmars@gmail.com>
2023-02-23 22:54:06 -0300

    Plugins pytest & autopep8 instalados

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,14 +4,16 @@ def test_check_type_to_dict():
 
     user = get_user()
 
-    assert type(user) == dict
+    assert dict == type(user)
+
+def test_check_type_name():
+
+    user = get_user()
+
+    assert str == type(user.get("name"))
 
 def test_check_name():
 
     user = get_user()
 
-    assert user.get("name") == "Sara Isabela Jennifer Porto"
-
-
-
-
+    assert "Sara Isabela Jennifer Porto" == user.get("name")
